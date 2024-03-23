8cd9c77744a381e5a8a7bd502469ddf87c85baef
crjacas <cesarrjacas1985@gmail.com>
2024-03-22 23:55:46 -0400

    Update run_workflow.sh

--- run_workflow.sh
+++ run_workflow.sh
@@ -10,4 +10,3 @@ model_correlation=0.95
 processors_numbers=1
 
 knime -consoleLog -nosplash -nosave -reset -application org.knime.product.KNIME_BATCH_APPLICATION -workflowFile="classification-QSAR-bioKom.knwf" -workflow.variable=PathTrainingFolder,$path_train,String -workflow.variable=PathTestFolder,$path_test,String -workflow.variable=MCC_Threshold,$mcc_threshold,double -workflow.variable=TargetFeature,$target_feature,String -workflow.variable=Feature_Entropy,$feature_entropy,double -workflow.variable=Feature_Correlation,$feature_correlation,double -workflow.variable=Model_Correlation,$model_correlation,double -workflow.variable=Processors,$processors_numbers,int -preferences=knime_preferences.epf
-
