4be9f06e19b85f4687f794e8f0becbd66601492b
crjacas <cesarrjacas1985@gmail.com>
2024-03-22 20:55:42 -0400

    Updating files

--- run_workflow.sh
+++ run_workflow.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-knime \
--consoleLog \
--nosplash \
--nosave \
--reset \
--application org.knime.product.KNIME_BATCH_APPLICATION \
--workflowFile="./classification-QSAR-bioKom.knwf" \
--workflow.variable=PathTrainingFolder,/opt/data/training/,String \
--workflow.variable=PathTestFolder,/opt/data/test/,String \
--workflow.variable=MCC_Threshold,0.5,double \
--workflow.variable=TargetFeature,Activity,String \
--workflow.variable=Feature_Entropy,0.25,double \
--workflow.variable=Feature_Correlation,0.95,double \
--workflow.variable=Model_Correlation,0.85,double \
--workflow.variable=Processors,1,int \
--preferences=knime_preferences.epf
+path_train=/opt/data/training/
+path_test=/opt/data/test/
+mcc_threshold=0.5
+target_feature=Activity
+feature_entropy=0.25
+feature_correlation=0.95
+model_correlation=0.95
+processors_numbers=1
+
+knime -consoleLog -nosplash -nosave -reset -application org.knime.product.KNIME_BATCH_APPLICATION -workflowFile="classification-QSAR-bioKom.knwf" -workflow.variable=PathTrainingFolder,$path_train,String -workflow.variable=PathTestFolder,$path_test,String -workflow.variable=MCC_Threshold,$mcc_threshold,double -workflow.variable=TargetFeature,$target_feature,String -workflow.variable=Feature_Entropy,$feature_entropy,double -workflow.variable=Feature_Correlation,$feature_correlation,double -workflow.variable=Model_Correlation,$model_correlation,double -workflow.variable=Processors,$processors_numbers,int -preferences=knime_preferences.epf
